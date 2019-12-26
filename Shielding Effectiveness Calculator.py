29e10cd6562fd0396576673ced55b07b7b31f8ae
Zach Vistuba <58920730+zvistuba@users.noreply.github.com>
2019-12-26 09:55:08 -0800

    Update Shielding Effectiveness Calculator.py

--- Shielding Effectiveness Calculator.py
+++ Shielding Effectiveness Calculator.py
@@ -1,10 +1,11 @@
 import math
-f1 = 100 * 10 ** 6 # frequancy is 100mhz
-fA = 401.65 * 10 ** 6 # Argos frequancy is 401.65mhz
-fGL1 = 1575.42 * 10 ** 6 # GPS frequancy is 1575.42mhz
+f1 = 100 * 10 ** 6 # frequancy is 100mhz in hz
+fA = 401.65 * 10 ** 6 # Argos frequancy is 401.65mhz in hz
+fGL1 = 1575.42 * 10 ** 6 # GPS frequancy is 1575.42mhz in hz 
 scu = 5.7 * 10 ** 7 #conductivity of copper in S/m
 ssn = 5.5 * 10 ** 7 #conductivity of nickel sliver 770 in S/m
 sT = 203.2 * 10 ** -6 #thickness of Shield in um converted to m
+
 
 
 def skin(s, f):
@@ -15,12 +16,24 @@ def shieldAB(t):
     """Calculates dB loss"""
     AdB = 8.7 * (t / sd)
     return AdB
-
+def wave(f):
+    """calculates wavelenght in meters"""
+    wl = 299792458 / f
+    return wl
+def shieldAP(w, l, n, k):
+    """claculates SE of apatures with a given wavelehtn, size of apature, number of apatures within half wavelngth and
+    k=20 for slots k=40 for holes (by defult)"""
+    se = k * math.log10( w / (2 * l)) - 20 * math.log10(n)
+    return se
 
 sd = skin(ssn, fGL1)
 db = shieldAB(sT)
+wl = wave(fGL1)
+ql = wl / 4
 
 print(f1, "Hz")
 print(scu, "S/m")
 print(sd * 10 ** 6, "um")
 print(db, "dB")
+print(wl, "wavelength")
+print(ql, "quarter wave")
